4990067e4cef3f29db20de298cf82860d847959e
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-04 12:46:22 +0000

    initiating routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import classes from './App.css';
@@ -10,14 +11,28 @@ const App = () => {
 
       <Header/>
 
-      {/*Tinder Cards*/}
+      <Router>
 
-      {/*Buttons below tinder cards*/}
+        <Switch>
+          <Route path="/chat">
+            <p>chatPage</p>
+          </Route>
+          <Route path="/">
+            <p>HomePage</p>
+          </Route>
+        </Switch>
+
+        {/*Tinder Cards*/}
+
+        {/*Buttons below tinder cards*/}
 
 
-      {/*Chats screen*/}
+        {/*Chats screen*/}
 
-      {/*Individual chat screen*/}
+        {/*Individual chat screen*/}
+      </Router>
+
+      
 
 
     </div>
