e1823e9ba026f722d39287585d674343ecf45b12
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-05 12:42:48 +0000

    adding a back button to the chat page

--- src/App.js
+++ src/App.js
@@ -15,19 +15,16 @@ const App = () => {
 
       <Router>
 
-        {/* even though this is always shown and therefore isn't a route,
-        this still has to be inside the Router/browserRouter component
-        we mentioned this in the react course and we actually get an error if we
-        have a navlink or a link directing somewhere inside this component and if it isn't inside the Router/browserRouter componen*/}
-        <Header/>
-
         <Switch>
 
           <Route path="/chat">
+            {/*if we pass a prop of back button, we want to replace the left icon in the header with an arrow with takes us to a previous page/route instead of the profile icon*/}
+            <Header backButton="/"/>
             <p>chatPage</p>
           </Route>
 
           <Route path="/">
+            <Header/>
             {/* when we swipe the cards off the screen, the component remains there, if we want to change this, we have to
             do this in the onCardLeftScreen callback, checkout documentation of this package here (https://www.npmjs.com/package/react-tinder-card) and look at th react course*/}
             <TinderCards/>
--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -10,19 +10,35 @@ icons around with this component to turn it into a button, this way we
 still have them as icons and add the functionalities of a button*/
 //note that when we do this, it already sets up a default animation on click, which we left it there as it was
 import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import {NavLink} from 'react-router-dom';
 
 import classes from './Header.css';
 
-const Header = () => {
+const Header = (props) => {
+
+  let leftIcon = (
+    <IconButton>
+      <PersonIcon className={classes.icon} fontSize="large"/>
+    </IconButton>
+  );
+
+  if(props.backButton) {
+    leftIcon = (
+      <NavLink to={props.backButton}>
+        <IconButton>
+          <ArrowBackIosIcon className={classes.icon} fontSize="large"/>
+        </IconButton>
+      </NavLink>
+    )
+  }
 
   return (
 
     <div className={classes.header}>
+      
+      {leftIcon}
 
-      <IconButton>
-        <PersonIcon className={classes.icon} fontSize="large"/>
-      </IconButton>
       <NavLink to="/">
         <img
           className={classes.logo}
