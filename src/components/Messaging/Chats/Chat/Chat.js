3463a2ef9d368920cc74337ce9f6508f5ab5e6e5
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-05 14:37:46 +0000

    adding navlinks to the Chat component so we can click and get into each private chat (ChatScreen component)

--- src/components/Messaging/Chats/Chat/Chat.js
+++ src/components/Messaging/Chats/Chat/Chat.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 import classes from './Chat.css';
 import Avatar from '@material-ui/core/Avatar';
 
 const Chat = (props) => {
   return (
-    <div className={classes.chat}>
-      {/* the cool thing about the Avatar from material UI is that if no profile pic is passed or if we have a problem loading it, it shows a generic profile icon*/}
-      <Avatar className={classes.chatImage} alt={props.name} src={props.profilePic}/>
-      <div className={classes.chatDetails}>
-        <h3>{props.name}</h3>
-        <p>{props.message}</p>
+    <NavLink to={"/chat/" + props.name}>
+      <div className={classes.chat}>
+        {/* the cool thing about the Avatar from material UI is that if no profile pic is passed or if we have a problem loading it, it shows a generic profile icon*/}
+        <Avatar className={classes.chatImage} alt={props.name} src={props.profilePic}/>
+        <div className={classes.chatDetails}>
+          <h3>{props.name}</h3>
+          <p>{props.message}</p>
+        </div>
+        <p className={classes.chatTimestamp}>{props.timestamp}</p>
       </div>
-      <p className={classes.chatTimestamp}>{props.timestamp}</p>
-    </div>
+    </NavLink>
   );
 };
 
