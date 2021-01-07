ea9777cec18a50344e34eafbe9f9ba3178874664
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-07 18:01:09 +0000

    keeping who disliked who and who was disliked by who in the case of a swipe left

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -83,6 +83,8 @@ export const userSwipedDirection = (direction) => {
 
 export const userSwiped = (direction, swipedUserId) => {
   return dispatch => {
+
+    //if the user swiped left
     if(direction === "left") {
 
       //getting the user that was swiped on so that we can grab the firebase key and save it.
@@ -103,14 +105,44 @@ export const userSwiped = (direction, swipedUserId) => {
           //now that we have the key saved in id, we can use the patch request to update the dislikedBy filed and use the key in the url
           //we will add the id of the logged in user to the dislikedBy field, therefore, we need to grab that id from the localStorage.
           const userId = localStorage.getItem("userId");
-          let urlParams = fetchedUsers[0].id 
+          const urlParams = fetchedUsers[0].id 
           axios.patch('https://tinder-9d380-default-rtdb.firebaseio.com/users/' + urlParams + '/dislikedBy.json', {[userId]: true})
             .then( response => {
-              dispatch(userSwipedDirection(direction));
+              //now we need to add to the logged in user saying that we swiped left on the shown user, for that we need the key to the object that represents the logged in user,
+              //for that we need to grab the logged in user
+              const queryParams1 = /*'?auth=' + token + '&*/'?orderBy="userId"&equalTo="' + userId + '"';
+              axios.get('https://tinder-9d380-default-rtdb.firebaseio.com/users.json' + queryParams1)
+
+                .then( response => {
+                  const authenticatedUser = [];
+                  for ( let key in response.data ) {
+                    authenticatedUser.push( {
+                      ...response.data[key],
+                      id: key
+                    });
+                  }
+
+                  //now that we have the firebase key, we need to to the logged in user saying that we disliked the user that was being shown
+                  const urlParams1 = authenticatedUser[0].id;
+                  axios.patch('https://tinder-9d380-default-rtdb.firebaseio.com/users/' + urlParams1 + '/disliked.json', {[swipedUserId]: true})
+
+                    .then( response => {
+                      dispatch(userSwipedDirection(direction));
+                    })
+                    .catch(err => {
+                      dispatch(fetchUsersFail(err));
+                    })
+
+                })
+
+                .catch( err => {
+                  dispatch(fetchUsersFail(err));
+                })
             })
+
             .catch( err => {
               dispatch(fetchUsersFail(err));
-            })
+            });
 
         })
         .catch( err => {
@@ -118,6 +150,8 @@ export const userSwiped = (direction, swipedUserId) => {
         });
       
     }
+
+    //if the user swiped right
     else {
       dispatch(userSwipedDirection(direction));
     }
