b25117e2d88541c9ba49f60ec647559ecdf93ce4
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-08 11:24:56 +0000

    keeping track of what user liked who and what user was liked by who in case of a swipe right

--- src/store/actions/users.js
+++ src/store/actions/users.js
@@ -153,6 +153,71 @@ export const userSwiped = (direction, swipedUserId) => {
 
     //if the user swiped right
     else {
+
+      //getting the user that was swiped on so that we can grab the firebase key and save it.
+      //adding the logged in user to the likedBy field on the swipedUserId
+      const queryParams = /*'?auth=' + token + '&*/'?orderBy="userId"&equalTo="' + swipedUserId + '"';
+      axios.get( 'https://tinder-9d380-default-rtdb.firebaseio.com/users.json' + queryParams)
+        .then( res => {
+          //even though we are only grabbing one user for sure, we still don't have a way to know the firebase key that comes in the response.
+          //that is why we still loop through something that we know that has only one key, that is why we still keep an array, just for good practice
+          const fetchedUsers = [];
+          for ( let key in res.data ) {
+            fetchedUsers.push( {
+              ...res.data[key],
+              id: key
+            });
+          }
+
+          //now that we have the key saved in id, we can use the patch request to update the likedBy filed and use the key in the url
+          //we will add the id of the logged in user to the likedBy field, therefore, we need to grab that id from the localStorage.
+          const userId = localStorage.getItem("userId");
+          const urlParams = fetchedUsers[0].id 
+          axios.patch('https://tinder-9d380-default-rtdb.firebaseio.com/users/' + urlParams + '/likedBy.json', {[userId]: true})
+            .then( response => {
+              //now we need to add to the logged in user saying that we swiped left on the shown user, for that we need the key to the object that represents the logged in user,
+              //for that we need to grab the logged in user
+              const queryParams1 = /*'?auth=' + token + '&*/'?orderBy="userId"&equalTo="' + userId + '"';
+              axios.get('https://tinder-9d380-default-rtdb.firebaseio.com/users.json' + queryParams1)
+
+                .then( response => {
+                  const authenticatedUser = [];
+                  for ( let key in response.data ) {
+                    authenticatedUser.push( {
+                      ...response.data[key],
+                      id: key
+                    });
+                  }
+
+                  //now that we have the firebase key, we need to to the logged in user saying that we liked the user that was being shown
+                  const urlParams1 = authenticatedUser[0].id;
+                  axios.patch('https://tinder-9d380-default-rtdb.firebaseio.com/users/' + urlParams1 + '/liked.json', {[swipedUserId]: true})
+
+                    .then( response => {
+                      dispatch(userSwipedDirection(direction));
+                    })
+                    .catch(err => {
+                      dispatch(fetchUsersFail(err));
+                    })
+
+                })
+
+                .catch( err => {
+                  dispatch(fetchUsersFail(err));
+                })
+            })
+
+            .catch( err => {
+              dispatch(fetchUsersFail(err));
+            });
+
+        })
+        .catch( err => {
+          dispatch(fetchUsersFail(err));
+        });
+
+
+
       dispatch(userSwipedDirection(direction));
     }
   }
