cf3ac029c4707270d99765d4ce1ce5ef1319cc73
DESKTOP-0LQ7HGV\migue <miguelnflopes98@gmail.com>
2021-01-15 11:41:21 +0000

    dynamic render of the header depending if the user is authenticated or not

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {NavLink} from 'react-router-dom';
 
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
@@ -11,7 +13,8 @@ still have them as icons and add the functionalities of a button*/
 //note that when we do this, it already sets up a default animation on click, which we left it there as it was
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import {NavLink} from 'react-router-dom';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 
 import classes from './Header.css';
 
@@ -33,6 +36,34 @@ const Header = (props) => {
     )
   }
 
+  //if the user isn't authenticated, we show the authentication button
+  let rightIcon = (
+    <NavLink to="/auth">
+        <IconButton>
+          <LockOpenIcon className={classes.icon} fontSize="large"/>
+        </IconButton>
+      </NavLink>
+  );
+
+  //if the user is authenticated, we show the logout button
+  if(props.isAuthenticated) {
+    rightIcon = (
+      <React.Fragment>
+        <NavLink to="/chat">
+          <IconButton>
+            <ForumIcon className={classes.icon} fontSize="large"/>
+          </IconButton>
+        </NavLink>
+
+        <NavLink to="/logout">
+          <IconButton>
+            <MeetingRoomIcon className={classes.icon} fontSize="large"/>
+          </IconButton>
+        </NavLink>
+      </React.Fragment>
+    );
+  }
+
   return (
 
     <div className={classes.header}>
@@ -46,15 +77,19 @@ const Header = (props) => {
           alt="tinderLogo"/>
       </NavLink>
 
-      <NavLink to="/chat">
-        <IconButton>
-          <ForumIcon className={classes.icon} fontSize="large"/>
-        </IconButton>
-      </NavLink>
+      
+
+      {rightIcon}
 
     </div>
 
   );
 };
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect( mapStateToProps )(Header);
